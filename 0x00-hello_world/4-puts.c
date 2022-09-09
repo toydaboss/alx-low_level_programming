018748975fddb08191e48b2403a473f95cddc90c
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-08 21:24:25 -0700

    Puts fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - Starting point
  *
- * Return - always return (0)
+ * Return - Always returns 0
  */
 
-int main(void){
+int main(void)
+{
 	puts ("\"Programming is like building a multilingual puzzle/n");
 	return(0);
 }
