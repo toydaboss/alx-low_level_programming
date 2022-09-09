fe133ad624da82164494c3259c209db8ea30fef0
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-08 21:16:18 -0700

    First C

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * Return - always return (0)
+ */
+
+int main(void){
+	put ("\"Programming is like building a multilingual puzzle/n");
+	return(0)
+}
