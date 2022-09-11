b85acb01116ceae96f4a76c8e0ae9cbae1389729
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-11 03:05:42 -0700

    adv task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  * followed by a new line, to the standard error.
  * Return: returns 0 for success
  */
-int main (void)
+int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
