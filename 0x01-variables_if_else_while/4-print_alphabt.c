6988e3e1faf7c506c9e386d7b7a3f97f9b9f9912
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-11 15:15:44 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - prints alphabet except for some letters
+  *
+  *Return: returns 0 for success
+  */
+int main(void)
+{
+	char x;
+
+	for ( x = 'a'; x <= 'z'; x++)
+	{
+		if ( x == 'e' || x == 'q' )
+			continue;
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
