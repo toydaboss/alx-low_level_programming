ea6e878657c1398edd7e0c8b6e197a7e9d23b4e4
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-11 14:06:44 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabeth.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - prints out the letters of the alphabeth
+  *
+  * Return: always return 0
+  */
+
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+		putchar(tolower(x));
+	return (0)
+}
