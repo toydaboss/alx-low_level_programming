0f71ae56568c32018383c8b0a91ea94b9b768866
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-12 04:47:35 -0700

    print number with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main - prints numbers in ascending order
+  *
+  *Return: returns 0 for sucess
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
