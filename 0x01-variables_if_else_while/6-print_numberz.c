f6a5640ebc8aa2c4e6314cf138afaba91a6986e2
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-12 03:41:25 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - prints numbers from 0-9
+  *
+  *Return: return 0 for success
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar("%d",i);
+	}
+	putchar("\n");
+	return (0);
+}
