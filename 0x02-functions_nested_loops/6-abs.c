d6a59e3be3c5fedbd0d489cf5b8acfdb1fd3645b
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-14 15:36:31 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *_abs - returns absolute value;
+  *@x: input value
+  *Return: returns 0
+  */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
