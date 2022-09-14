0efbedb91aeab864eea9526fe7539c37710fbef8
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-14 14:45:04 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *_isalpha - checks alphabet
+  *
+  *Return: returns 0 for success
+  */
+
+int _isalpha(int c)
+{
+	char r, t;
+	int alpha;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+		if (r == c)
+			lower = 1;
+	}
+	for (t = 'A'; t <= 'Z'; t++)
+	{
+		if (t == c)
+			lower = 1;
+	}
+	return (lower);
+}
