3dc0e91f75b058eae0c78be8c305e64abeff8ba8
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-14 21:02:58 -0700

    9 times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 			{
 				if (j != 0)
 					_putchar(' ');
-					_putchar(n + '0');
+				_putchar(n + '0');
 				if (j == 9)
 					continue;
 				_putchar(',');
@@ -37,5 +37,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-
 }
