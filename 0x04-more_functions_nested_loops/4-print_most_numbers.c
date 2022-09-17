58961ac6c9b7126862037c707318c068466f55da
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-16 21:20:57 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *print_most_numbers - prints numbers
+  *
+  *Return: returns void
+  */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		_putchar(i);
+	}
+	_putchar(i);
+}
