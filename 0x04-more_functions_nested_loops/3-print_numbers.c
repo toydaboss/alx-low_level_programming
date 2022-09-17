e24435488f35ce98793a525cce5e5c7ea266a82e
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-16 21:06:38 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *print_numbers - prints numbers
+  *
+  *Return: returns 0
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
