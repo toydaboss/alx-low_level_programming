4336a9dd277a5c1f4cff00081e92af8a4d74acb3
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-22 20:14:43 -0700

    strn cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * *_strncat - concatenates n bytes of two strings
+  * @dest: pointer destination
+  * @src: pointer source
+  * @n: number of bytes
+  * Return: void
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int len, i;
+	for (len = 0; dest[len] != '\0'; len++);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+
+
+	dest[len + i] = '\0';
+
+	return (dest);
+}
