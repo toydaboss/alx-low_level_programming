315433e38a01c7bc563ee3aafd385321a6858757
toydaboss <toyeseaderibigbe@gmail.com>
2022-09-22 20:20:36 -0700

    strn concat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,10 @@
 char *_strncat(char *dest, char *src, int n)
 {
 
-	int len, i;
-	for (len = 0; dest[len] != '\0'; len++);
+	int len = 0, i;
 
+	while (des[len] != '\0')
+		len++;
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[len + i] = src[i];
 
