5625461bff1a19fdd6c8ac9e32e0cf904aa5b1bb
toydaboss <toyeseaderibigbe@gmail.com>
2022-10-04 21:29:56 -0700

    free grind

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,3 +2,4 @@ char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
+void free_grid(int **grid, int height);
