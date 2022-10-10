d0560c37277fc4139efca79a846a46f270f08b69
toydaboss <toyeseaderibigbe@gmail.com>
2022-10-10 06:44:14 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+  * struct dog - structure for a basic dog
+  * @name: Dog's name
+  * @age: Dog's age
+  * @owner: Dog's owner
+  * Description: data structure for dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
