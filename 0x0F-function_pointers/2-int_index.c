af13e8b2cc3f8de5069c3c471b9669f22896a8e4
toydaboss <toyeseaderibigbe@gmail.com>
2022-10-12 21:09:30 -0700

    size fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -23,8 +23,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]))
 			return (i);
 
-		if (i == size)
-			return (-1);
-
+	if (i == size)
 		return (-1);
+
+	return (-1);
 }
