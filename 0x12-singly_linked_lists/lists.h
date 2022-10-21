ae2afc957b931537c4d535cec47df9af4dd25f20
toydaboss <toyeseaderibigbe@gmail.com>
2022-10-20 20:36:33 -0700

    linked list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,32 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ *
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *              for Alx project
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const lis_t *h);
+
+#endif /* LISTS_H */
