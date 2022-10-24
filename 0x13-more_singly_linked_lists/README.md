e4d07139f42bda671ce8f12c2bfa447e9a31b49e
toydaboss <toyeseaderibigbe@gmail.com>
2022-10-24 11:49:15 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Task for more_singly_linked_lists
---
+++ 0x13-more_singly_linked_lists/list.h
@@ -0,0 +1,22 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+#include <stdlib.h>
+
+/**
+  * struct listint_s - singly linked list
+  * @n: integer
+  * @next: points to the next node
+  *
+  * Description: singly linked list node structure
+  * 
+  */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t recursion_printer(const listint_t *h);
+
+#endif /* _LISTS_H_ */
