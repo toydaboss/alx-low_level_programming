6cb8406983713275745585dbb7f6179e8dbd1663
toydaboss <toyeseaderibigbe@gmail.com>
2022-12-12 10:41:19 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *_strlen - for length of a string
+  *@s: input
+  *Return: returns int
+  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
